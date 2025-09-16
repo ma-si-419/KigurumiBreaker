36d5e4c25144acec55751ecf1fff478c48bf4f18
ma-si- <Kouma9140@gmail.com>
2025-09-16 15:56:42 +0900

    攻撃の挙動仮作成

---
+++ KigurumiBreaker/Assets/Script/Player/ZangiMove.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZangiMove : MonoBehaviour
+{
+
+    public int hp = 30;
+
+    private bool isDamage = false;
+
+    int time = 0;
+
+    void FixedUpdate()
+    {
+        if (isDamage)
+        {
+            time++;
+
+            if (time > 15)
+            {
+                isDamage = false;
+            }
+        }
+        else
+        {
+            time = 0;
+            
+            this.GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerAttack")
+        {
+
+            hp -= other.gameObject.GetComponent<PlayerAttack>().GetDamage();
+
+            Debug.Log(other.gameObject.GetComponent<PlayerAttack>().GetDamage() + "ÇÃÉ_ÉÅÅ[ÉW");
+
+            isDamage = true;
+
+            time = 0;
+
+            this.GetComponent<Renderer>().material.color = Color.red;
+
+            if (hp <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
