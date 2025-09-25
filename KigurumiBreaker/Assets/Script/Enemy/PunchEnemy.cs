047eeb4d359a3f59934a00fa7bd70d443703631f
4zu1Q <Sota710794@i.softbank.jp>
2025-09-25 16:50:38 +0900

    up

--- KigurumiBreaker/Assets/Script/Enemy/PunchEnemy.cs
+++ KigurumiBreaker/Assets/Script/Enemy/PunchEnemy.cs
@@ -8,24 +8,20 @@ public class PunchEnemy : Enemy
     private bool _isAttacking = false;  // 攻撃中かどうかのフラグ
     private bool _isDash = false;       // ダッシュ中かどうかのフラグ
 
-    [SerializeField] private float _attackDuration = 1.0f;  // 攻撃全体の長さ
-    [SerializeField] private float _hitTime = 0.5f;         // 攻撃がヒットするタイミング
-    [SerializeField] private float _attackRange = 1.5f;     // 攻撃の有効範囲
-    [SerializeField] private float _dashTime  = 0.25f;       // 攻撃のダメージ量
-    [SerializeField] private float _dashDistance = 1.0f;       // ダッシュ距離
-    [SerializeField] private float _dashSpeed = 10f;        // ダッシュの速さ
-
-    private Rigidbody _rigidbody;
+    private Rigidbody _rigidbody;   // Rigidbody
 
     public override void Attack()
     {
         //base.Attack();
+        
 
         // タイマーを更新
+        _punchTimer += Time.deltaTime;
+        //Collider[] hits = Physics.OverlapSphere(transform.position + transform.forward, 1f, LayerMask.GetMask("Player"));
 
-
-
+           
         //攻撃判定を行う
+
         transform.LookAt(player.transform); //プレイヤーの方向を向く
 
 
