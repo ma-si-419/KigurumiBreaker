939d417316383f58d1f3097b098adcdcf92a64f6
4zu1Q <Sota710794@i.softbank.jp>
2025-09-25 12:40:35 +0900

    up

---
+++ KigurumiBreaker/Assets/Script/Enemy/EnemyBaseFolder/DeadState.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadState : IState
+{
+    private Enemy _enemy;   //敵の参照
+
+    public DeadState(Enemy enemy)
+    {
+        //コンストラクタでEnemyの参照を受け取る
+        _enemy = enemy; 
+    }   
+
+    public void Init()
+    {
+        Debug.Log("AttackState: Init");
+    }
+
+    public void Update()
+    {
+        //タイマーを進める
+        Debug.Log("AttackState: Update");
+
+        //死んだ処理書いとけ
+    }
+
+    public void End()
+    {
+        Debug.Log("AttackState: End");
+    }
+}
---
+++ KigurumiBreaker/Assets/Script/Enemy/TackleEnemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TackleEnemy : Enemy
+{ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+
+        // ここにタックル攻撃の具体的な処理を追加
+        Debug.Log("PunchEnemy: Performing punch attack!");
+
+        
+    }
+
+}
