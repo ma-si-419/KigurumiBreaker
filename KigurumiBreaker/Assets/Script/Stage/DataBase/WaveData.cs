38b6b67c5d5f2133b393cd459c8faf5ea8551803
itukikunmeuu <135081677+itukikunmeuu@users.noreply.github.com>
2025-10-31 14:20:52 +0900

    up

--- KigurumiBreaker/Assets/Script/Stage/DataBase/WaveData.cs
+++ KigurumiBreaker/Assets/Script/Stage/DataBase/WaveData.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "SpawnEnemyData", menuName = "GameData/SpawnEnemyData")]
-public class SpawnEnemyData : ScriptableObject
+public class WaveData : ScriptableObject
+{
+    [Header("敵の配置データ(いくつ配置データを持たせるか)")]
+    public List<EnemyPopGroup> waveEnemyDataList = new List<EnemyPopGroup>();
+}
+
+[System.Serializable]
+public class EnemyPopGroup
 {
     [Header("敵の出現データ（ウェーブと敵情報まとめ）")]
-    public List<WaveEnemyData> waveEnemyDataList = new List<WaveEnemyData>();
+    [SerializeField] private List<WaveEnemyData> _spawnDataList; // ← SpawnEnemyData のリストに
+
+    [HideInInspector][SerializeField] int _index = 0;
+    public void SetIndex(int index) { _index = index; }
+
+    public int index => _index;
+    public List<WaveEnemyData> spawnDataList => _spawnDataList;
 }
 
 [System.Serializable]
 public class WaveEnemyData
 {
+    [Header("各敵グループの出現順データ")]
     public List<PopEnemyData> popEnemies = new List<PopEnemyData>();
 }
 
@@ -21,3 +35,4 @@ public class PopEnemyData
     public Vector3 spawnPosition;
     public bool randomizePosition = true;
 }
+
