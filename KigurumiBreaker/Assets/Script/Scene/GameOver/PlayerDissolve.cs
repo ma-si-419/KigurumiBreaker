8dc42ad2bc7a0d54aec13e5834097c3c987758bf
GuniruPanjan <135081686+GuniruPanjan@users.noreply.github.com>
2026-01-13 12:28:34 +0900

    up

--- KigurumiBreaker/Assets/Script/Scene/GameOver/PlayerDissolve.cs
+++ KigurumiBreaker/Assets/Script/Scene/GameOver/PlayerDissolve.cs
@@ -7,9 +7,12 @@ public class PlayerDissolve : MonoBehaviour
     [SerializeField] private float _Time = 1.0f;       //再生時間
     [SerializeField] private float _WaitTime = 1.0f;   //再生までの待ち時間
 
-    private Material _Material = null; 
+    private Material _Material = null;
+    [SerializeField] private Material _OutLineMaterial = null;
     private int _Width = 0;
     private int _Cutoff = 0;
+    private int _ColorIntensity = 0;
+    private int _OutLineAlpha = 0;
 
     private float _Duration = 0.0f;   //残り時間
     private float _HalfTime = 0.0f;   //再生時間の半分
@@ -20,11 +23,15 @@ public class PlayerDissolve : MonoBehaviour
         _Material = GetComponentInChildren<Renderer>().material;
         _Width = Shader.PropertyToID("_Width");
         _Cutoff = Shader.PropertyToID("_CutOff");
+        _ColorIntensity = Shader.PropertyToID("_ColorIntensity");
+        _OutLineAlpha = Shader.PropertyToID("_Alpha");
 
         if(_Material != null)
         {
             _Material.SetFloat(_Cutoff, 1.0f);
             _Material.SetFloat(_Width, 1.0f);
+            _Material.SetFloat(_ColorIntensity, 0.0f);
+            _OutLineMaterial.SetFloat(_OutLineAlpha, 1.0f);
         }
         _HalfTime = _Time / 4.0f * 3.0f;   //見た目調整
         _Duration = _Time;
@@ -48,16 +55,18 @@ public class PlayerDissolve : MonoBehaviour
         if (cutoff < 0.0f) cutoff = 0.0f;
 
         //幅のアニメーション(再生時間の下半分の時間で1～0に推移)
-        //float width = (_HalfTime - _Duration) - _HalfTime;
-        float width = (_Duration - _HalfTime) / _HalfTime;
+        float width = (_HalfTime - _Duration) / _HalfTime;
+        //float width = (_Duration - _HalfTime) / _HalfTime;
         if (width < 0.0f) width = 0.0f;
-        //width = 1.0f - width;
+        width = 1.0f - width;
 
         //シェーダーに値を渡す
         if(_Material != null)
         {
             _Material.SetFloat(_Cutoff, cutoff);
             _Material.SetFloat(_Width, width);
+            _Material.SetFloat(_ColorIntensity, width);
+            _OutLineMaterial.SetFloat(_OutLineAlpha, cutoff);
         }
 
     }
