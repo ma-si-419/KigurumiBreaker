af8249f4fe1bbe8ff33ebc0ddcf6fbc43867f6f8
4zu1Q <Sota710794@i.softbank.jp>
2026-01-14 15:20:17 +0900

    up

--- KigurumiBreaker/Assets/Script/Ui/HpBarController.cs
+++ KigurumiBreaker/Assets/Script/Ui/HpBarController.cs
@@ -8,11 +8,23 @@ public class HpBarController : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
 
-    [SerializeField] private Image _hpGaugeImage;
+    // 現在のHpを表示するImage
+    [SerializeField] private Image _currentHpGaugeImage;
+    // 遅れて減る赤バー
+    [SerializeField] private Image _delayedHpGaugeImg;
+
     [SerializeField] private TMP_Text _hpText;
-    [SerializeField] private TMP_Text _hpMaxText;
+    [SerializeField] private TMP_Text _shadowHpText;
 
     private PlayerState _playerState;
+
+    // 硬直時間
+    private float _delayRigidityTime = 0.5f;
+    // 補間速度
+    private float _lerpSpeed = 1.5f;
+
+    // コルーチン管理用
+    private Coroutine _hpCoroutine;
 
     private float _currentHp = 0f;    // 現在の体力
     private float _maxHp = 100f;      // 最大体力
@@ -23,37 +35,73 @@ public class HpBarController : MonoBehaviour
         // 初期化
         _playerState = _player.GetComponent<PlayerState>();
         _currentHp = _playerState.GetNowHp();
-        _maxHp = _playerState.GetMaxHp();
+
+        _currentHpGaugeImage.fillAmount = _playerState.GetNowHp() / _playerState.GetMaxHp();
+        _delayedHpGaugeImg.fillAmount = _currentHpGaugeImage.fillAmount;
+
     }
 
     // Update is called once per frame
     private void Update()
     {
-
         // 現在のHPと最大HPを取得
         _currentHp = _playerState.GetNowHp();
         _maxHp = _playerState.GetMaxHp();
 
+        float targetHpRatio = _currentHp / _maxHp;
+
         // HPゲージの更新
-        _hpGaugeImage.fillAmount = _currentHp / _maxHp;
+        //_currentHpGaugeImage.fillAmount = _currentHp / _maxHp;
+
         // HPテキストの更新
         _hpText.text = ((int)_currentHp).ToString();
-        _hpMaxText.text = ((int)_maxHp).ToString();
+        _shadowHpText.text = ((int)_currentHp).ToString();
+
+        // 現在のHpが目標値と異なる場合
+        if (_currentHpGaugeImage.fillAmount != targetHpRatio)
+        {
+            // 即座に減らす
+            _currentHpGaugeImage.fillAmount = targetHpRatio;
+
+            // 遅延バーの減少をストップ
+            if (_hpCoroutine != null) StopCoroutine(_hpCoroutine);
+
+            // 遅延バーの減少を開始
+            _hpCoroutine = StartCoroutine(HpDelayDecrease(targetHpRatio));
+
+        }
 
         // HPの割合に応じて色を変更
         float ratio = _currentHp / _maxHp;
 
         if (ratio > 0.5f)
         {
-            _hpGaugeImage.color = Color.green;
+            Color color = new Color32(71, 200, 100, 255);
+            _currentHpGaugeImage.color = color;
         }
         else if (ratio > 0.2f)
         {
-            _hpGaugeImage.color = Color.yellow;
+            _currentHpGaugeImage.color = new Color32(229, 145, 42, 255);
         }
         else
         {
-            _hpGaugeImage.color = Color.red;
+            _currentHpGaugeImage.color = new Color32(229, 49, 49, 255);
         }
+    }
+
+    private IEnumerator HpDelayDecrease(float target)
+    {
+        // 1秒硬直
+        yield return new WaitForSeconds(_delayRigidityTime);
+
+        // ゆっくり減少
+        while (_delayedHpGaugeImg.fillAmount > target)
+        {
+            _delayedHpGaugeImg.fillAmount -= Time.deltaTime * _lerpSpeed;
+            yield return null;
+        }
+
+        // 目標値に合わせる
+        _delayedHpGaugeImg.fillAmount = target;
     }
 }
