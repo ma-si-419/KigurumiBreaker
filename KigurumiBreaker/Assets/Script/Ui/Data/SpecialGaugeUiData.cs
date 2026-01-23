0c6c55f56ae248425ba3351fc137b592c8a59039
ma-si- <Kouma9140@gmail.com>
2026-01-23 10:59:28 +0900

    up

--- KigurumiBreaker/Assets/Script/Ui/Data/SpecialGaugeUiData.cs
+++ KigurumiBreaker/Assets/Script/Ui/Data/SpecialGaugeUiData.cs
@@ -9,14 +9,15 @@ public class SpecialGaugeUiData : ScriptableObject
     [SerializeField] private Color NormalColor = Color.yellow;
     [Header("たまったときのゲージの色")]
     [SerializeField] private Color MaxColor = Color.red;
-    [Header("点滅速度")]
-    [SerializeField] private float FlashSpeed = 5f;
-    [Header("オーラの回転速度")]
-    [SerializeField] private float AuraRotateSpeed = 50f;
+    [Header("後ろの炎の通常時の色")]
+    [SerializeField] private Color AuraNormalColor = new Color(0.2f, 0.2f, 0.2f, 1.0f);
+    [Header("後ろの炎の最大時の色")]
+    [SerializeField] private Color AuraMaxColor = new Color(0.8f, 0f, 0f, 1.0f);
+
 
     // 読み取り専用
     public Color normalColor => NormalColor;
     public Color maxColor => MaxColor;
-    public float flashSpeed => FlashSpeed;
-    public float auraRotateSpeed => AuraRotateSpeed;
+    public Color auraNormalColor => AuraNormalColor;
+    public Color auraMaxColor => AuraMaxColor;
 }
