3713d55a6bf2339f61de963e8691a517b7522fa1
ma-si- <Kouma9140@gmail.com>
2026-01-31 20:34:37 +0900

    バグ修正

---
+++ KigurumiBreaker/Assets/Script/Ui/FadeOut.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour
+{
+    [SerializeField] private GameObject _sprite;
+
+    [SerializeField] private float _fadeOutTime = 120f;
+
+    bool _isFadeOut = false;
+
+    private Image _image;
+
+    private void Start()
+    {
+        _image = _sprite.GetComponent<Image>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isFadeOut)
+        {
+            Color color = _image.color;
+            color.a += 1.0f / _fadeOutTime;
+            _image.color = color;
+            if (color.a >= 1f)
+            {
+                _isFadeOut = false;
+
+                BaseSceneController.instance.ChangeSceneWithFade(SceneType.GameOverScene);
+
+            }
+        }
+    }
+
+    public void StartFadeOut()
+    {
+        if (_isFadeOut) return;
+        _isFadeOut = true;
+    }
+
+}
