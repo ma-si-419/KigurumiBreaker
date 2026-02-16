73315646d3caaea0120156ef25c5e16f80396a01
ma-si- <Kouma9140@gmail.com>
2026-02-16 09:59:30 +0900

    p

--- KigurumiBreaker/Assets/Script/Stage/GoalPoint.cs
+++ KigurumiBreaker/Assets/Script/Stage/GoalPoint.cs
@@ -6,18 +6,24 @@ public class GoalPoint : MonoBehaviour
     private StageSpawner _stageSpawner;
     [SerializeField] private int goalIndex;
     [SerializeField] private WaveSpawner waveSpawner; // Å© SerializeField Ç…Ç∑ÇÈ
+    private bool _isGoal;
 
     void Start()
     {
+        _isGoal = false;
+
         _stageSpawner = FindObjectOfType<StageSpawner>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (_isGoal) return;
+
         if (other.CompareTag("Player") && _stageSpawner != null)
         {
             waveSpawner.OnGoalReached(goalIndex);
             _stageSpawner.NextStage();
+            _isGoal = true; // ÉSÅ[ÉãÇ…ìûíBÇµÇΩÇ±Ç∆ÇãLò^
         }
     }
 }
